f794c8223480134836ae0576825c0608d32671fe
Guilhermo Masid <guilhermomasid@gmail.com>
2021-01-12 14:56:18 -0300

    new challenge completed - is sum of consecutive

---
+++ 2 - The Core/6 - Labyrinth od Nested Loops/isPower.js
@@ -0,0 +1,14 @@
+function isPower(n) {
+  for(let i = 0; i <= 20; i++) {
+    for(let j = 2; j <= 20; j++) {
+      if(Math.pow(i, j) === n) {
+        console.log(i, j)
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
+console.log(isPower(11))
