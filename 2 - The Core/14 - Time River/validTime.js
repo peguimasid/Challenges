a0a9fe5091bb231a208af0b5f368671a0f70e579
Guilhermo Masid <guilhermomasid@gmail.com>
2021-02-24 11:52:00 -0300

    new challange completed - is valid time / correction

--- 2 - The Core/14 - Time River/validTime.js
+++ 2 - The Core/14 - Time River/validTime.js
@@ -2,7 +2,7 @@ function validTime(time) {
   let hours = time.split(':')[0]
   let minutes = time.split(':')[1]
 
-  return hours < 23 && minutes <= 59
+  return hours < 24 && minutes < 60
 }
 
 console.log(validTime("13:58"))
