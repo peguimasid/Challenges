6a4e266c49f5b246982d74890d96fbd75e890798
Guilhermo Masid <guilhermomasid@gmail.com>
2021-03-23 16:17:11 -0300

    new challange completed - calculating with functions

---
+++ CodeWars.com/calculatingWithFunctions.js
@@ -0,0 +1,21 @@
+function proc(num, fn) {
+  return (fn) ? fn.call(fn, num) : num
+}
+
+function zero(fn) { return proc(0, fn) }
+function one(fn) { return proc(1, fn) }
+function two(fn) { return proc(2, fn) }
+function three(fn) { return proc(3, fn) }
+function four(fn) { return proc(4, fn) }
+function five(fn) { return proc(5, fn) }
+function six(fn) { return proc(6, fn) }
+function seven(fn) { return proc(7, fn) }
+function eight(fn) { return proc(8, fn) }
+function nine(fn) { return proc(9, fn) }
+
+function plus(num) { return function (res) { return res + num } }
+function minus(num) { return function (res) { return res - num } }
+function times(num) { return function (res) { return res * num } }
+function dividedBy(num) { return function (res) { return res / num } }
+
+console.log(eight(plus(nine())))
