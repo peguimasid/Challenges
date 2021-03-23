f9fc16ee175ae40952185f7d7eee67a7d6dbc6f7
Guilhermo Masid <guilhermomasid@gmail.com>
2021-03-23 16:18:21 -0300

    changes

--- CodeWars.com/calculatingWithFunctions.js
+++ CodeWars.com/calculatingWithFunctions.js
@@ -13,9 +13,9 @@ function seven(fn) { return proc(7, fn) }
 function eight(fn) { return proc(8, fn) }
 function nine(fn) { return proc(9, fn) }
 
-function plus(num) { return function (res) { return res + num } }
-function minus(num) { return function (res) { return res - num } }
-function times(num) { return function (res) { return res * num } }
-function dividedBy(num) { return function (res) { return res / num } }
+function plus(num) { return function (res) { return Math.floor(res + num) } }
+function minus(num) { return function (res) { return Math.floor(res - num) } }
+function times(num) { return function (res) { return Math.floor(res * num) } }
+function dividedBy(num) { return function (res) { return Math.floor(res / num) } }
 
 console.log(eight(plus(nine())))
