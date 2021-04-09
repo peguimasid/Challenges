4e67746e7f655b5353e3792b979aaf14c063c5ae
Guilhermo Masid <guilhermomasid@gmail.com>
2021-04-09 11:27:26 -0300

    fix typing error

--- HackerRank.com/Algorithms/billDivision.js
+++ HackerRank.com/Algorithms/billDivision.js
@@ -1,11 +1,11 @@
 function bonAppetit(bill, k, b) {
-  const annaShoudPay = bill.reduce((acc, curr, index) => {
+  const annaShouldPay = bill.reduce((acc, curr, index) => {
     return index === k ? acc + 0 : acc + curr
   }) / 2
 
   const annaPayed = b
 
-  return annaPayed - annaShoudPay === 0 ? 'Bon Appetit' : annaPayed - annaShoudPay
+  return annaPayed - annaShouldPay === 0 ? 'Bon Appetit' : annaPayed - annaShouldPay
 }
 
-console.log(bonAppetit([3, 10, 2, 9], 1, 7))
+console.log(bonAppetit([3, 10, 2, 9], 1, 14))
