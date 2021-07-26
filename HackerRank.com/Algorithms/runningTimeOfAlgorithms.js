d1c1e24d58eaf54cb9165990bd4abd9590f882d6
Guilhermo Masid <guilhermomasid@gmail.com>
2021-07-26 11:02:57 -0300

    new challange completed - running time of algorithms

---
+++ HackerRank.com/Algorithms/runningTimeOfAlgorithms.js
@@ -0,0 +1,20 @@
+function runningTime(array) {
+  let result = 0;
+
+  for (let i = 1; i < array.length; i++) {
+    const currentVal = array[i];
+
+    for (var j = i - 1; j >= 0 && array[j] > currentVal; j--) {
+      array[j + 1] = array[j];
+      result++;
+    }
+
+    array[j + 1] = currentVal;
+
+    temp = [...array];
+  }
+
+  return result;
+}
+
+console.log(runningTime([2, 1, 3, 1, 2]));
