adbc1405cebb3ce57b50fb8f0a2ab4dd934261cc
Guilhermo Masid <guilhermomasid@gmail.com>
2022-01-07 10:42:24 -0300

    new challange completed - simple remove duplicate HARD SOL

--- CodeWars.com/simpleRemoveDuplicates.js
+++ CodeWars.com/simpleRemoveDuplicates.js
@@ -1,5 +1,11 @@
 function solve(arr) {
-  return [...new Set(arr.reverse())].reverse();
+  let result = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (!result.includes(arr[i])) result.unshift(arr[i]);
+  }
+
+  return result;
 }
 
 console.log(solve([3, 4, 4, 3, 6, 3]));
