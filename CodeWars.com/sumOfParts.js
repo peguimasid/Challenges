8373f24ca5072f5055e84664db46b1be8e1e07d7
Guilhermo Masid <guilhermomasid@gmail.com>
2022-02-11 11:51:47 -0300

    new challange completed - sum of parts

---
+++ CodeWars.com/sumOfParts.js
@@ -0,0 +1,19 @@
+function partsSums(ls) {
+  ls.unshift(0);
+  let sum = ls.reduce((acc, curr) => acc + curr, 0);
+  return ls.map((item) => (sum = sum - item));
+  console.log(ls, sum);
+}
+
+console.log(partsSums([0, 1, 3, 6, 10]));
+// console.log(
+//   partsSums([744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358])
+// );
+
+// [0, 1, 3, 6, 10]
+// [1, 3, 6, 10]
+// [3, 6, 10]
+// [6, 10]
+// [10]
+// []
+// [20 20, 19, 16, 10, 0]
