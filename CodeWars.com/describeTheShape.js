7c04f0355f84ea7d5f36d44d2975d4c96a7dc106
Guilhermo Masid <guilhermomasid@gmail.com>
2022-03-09 10:59:16 -0300

    new challange completed - describe the shape

---
+++ CodeWars.com/describeTheShape.js
@@ -0,0 +1,14 @@
+function describeTheShape(angles) {
+  if (angles <= 2) return 'this will be a line segment or a dot';
+
+  const angle = ((angles - 2) * 180) / angles;
+
+  return `This shape has ${angles} sides and each angle measures ${angle}`;
+}
+
+console.log(describeTheShape(6)); // "This shape has 6 sides and each angle measures 120"
+console.log(describeTheShape(3)); // "This shape has 3 sides and each angle measures 60"
+console.log(describeTheShape(8)); // "This shape has 8 sides and each angle measures 135"
+console.log(describeTheShape(90)); // "This shape has 90 sides and each angle measures 176"
+console.log(describeTheShape(2)); // "this will be a line segment or a dot"
+console.log(describeTheShape(1)); // "this will be a line segment or a dot"
