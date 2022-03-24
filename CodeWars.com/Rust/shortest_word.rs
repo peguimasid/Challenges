5fbdb91fab97badee862c3468ed929d388237964
Guilhermo Masid <guilhermomasid@gmail.com>
2022-03-24 17:49:10 -0300

    new challange completed - shortest word

---
+++ CodeWars.com/Rust/shortest_word.rs
@@ -0,0 +1,14 @@
+fn find_short(s: &str) -> u32 {
+  let mut result = std::u32::MAX;
+
+  for word in s.split_ascii_whitespace() {
+    let len = word.chars().count().try_into().unwrap();
+    if len < result { result = len }
+  }
+
+  result
+}
+
+fn main() {
+  println!("{}", find_short("bitcoin take over the world maybe who knows perhaps"));
+}
