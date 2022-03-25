e6db69d66ad282bc3159beee92d9757d7e47f1fd
Guilhermo Masid <guilhermomasid@gmail.com>
2022-03-25 10:49:44 -0300

    new challange completed - dont give me five - FILTER SOLUTION

--- CodeWars.com/Rust/dont_give_me_five.rs
+++ CodeWars.com/Rust/dont_give_me_five.rs
@@ -1,15 +1,5 @@
 fn dont_give_me_five(start: isize, end: isize) -> isize {
-  let mut result: isize = 0;
-
-  for i in start..end + 1 {
-    let includes_five = i.to_string().contains('5');
-
-    if !includes_five {
-      result += 1;
-    }
-  }
-
-  result
+  (start..end + 1).filter(|x| !x.to_string().contains('5')).count() as isize
 }
 
 fn main() {
