e6d6a07a599020345bcfd4d8dd28d5f38e956584
Guilhermo Masid <guilhermomasid@gmail.com>
2022-06-09 11:22:22 -0300

    running in On time

--- LeetCode.com/dailyProblems/twoSum2InputArraySorted.js
+++ LeetCode.com/dailyProblems/twoSum2InputArraySorted.js
@@ -4,18 +4,21 @@
  * @return {number[]}
  */
 const twoSum = (numbers, target) => {
+  let pair = 0;
+  let prev = Infinity;
   let i = 0;
-  let j = 1;
 
-  while (i < numbers.length && numbers[i] <= target) {
-    j = i + 1;
-    while (numbers[i] + numbers[j] <= target) {
-      if (numbers[i] + numbers[j] === target) return [++i, ++j];
-      j++;
+  while (i < numbers.length) {
+    if (numbers[i] !== prev) {
+      pair = numbers.indexOf(target - numbers[i], i + 1);
+      if (pair >= 0) return [i + 1, pair + 1];
     }
+
+    prev = numbers[i];
     i++;
   }
 };
 
 console.log(twoSum([-3, 3, 4, 90], 0));
 console.log(twoSum([-1, 0], -1));
+console.log(twoSum([5, 25, 75], 100));
