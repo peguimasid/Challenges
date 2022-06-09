3538bcfd69a35612ec74c5b4a6a14ce467751e64
Guilhermo Masid <guilhermomasid@gmail.com>
2022-06-09 10:58:30 -0300

    new challenge completed - two sum input array sorted On^2 solution

---
+++ LeetCode.com/dailyProblems/twoSum2InputArraySorted.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (numbers, target) => {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) return [++i, ++j];
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 3, 4], 6));
+console.log(twoSum([-1, 0], -1));
