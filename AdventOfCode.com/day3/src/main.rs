8b3c53628fcab54df9c05e9bdd31b0414f6f9d1a
Guilhermo Masid <guilhermomasid@gmail.com>
2022-07-08 11:13:04 -0300

    printing all numbers verticaly

--- AdventOfCode.com/day3/src/main.rs
+++ AdventOfCode.com/day3/src/main.rs
@@ -1,3 +1,31 @@
+impl BinaryDiagnostic {
+  fn solution(diagnostic: Vec<&str>) -> i32 {
+    // println!("{:?}", diagnostic[0].chars().nth(0).unwrap());
+    // println!("{:?}", diagnostic[1].chars().nth(0).unwrap());
+    // println!("{:?}", diagnostic[2].chars().nth(0).unwrap());
+    let mut result = Vec::<i32>::new();
+
+    for i in 0..diagnostic[0].chars().count() {
+      for j in 0..diagnostic.len() {
+        println!(
+          "{} <<>> i = {} | j = {}",
+          diagnostic[j].chars().nth(i).unwrap(),
+          i,
+          j
+        );
+      }
+    }
+
+    // println!("{:?}", result);
+    42
+  }
+}
+struct BinaryDiagnostic;
+
 fn main() {
-  println!("Hello, world!");
+  let diagnostic = vec![
+    "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001",
+    "00010", "01010",
+  ];
+  println!("{}", BinaryDiagnostic::solution(diagnostic));
 }
