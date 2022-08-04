8d68a3d7022780fa2057010df9994344fa97f31f
Guilhermo Masid <guilhermomasid@gmail.com>
2022-08-04 11:28:57 -0300

    new challenge completed - buddy strings - good Big(o)

--- LeetCode.com/problems/buddyStrings.js
+++ LeetCode.com/problems/buddyStrings.js
@@ -1,27 +1,44 @@
-const swap = (string, i, j) => {
-  return string
-    .split('')
-    .map((char, index) => {
-      if (index === i) return string[j];
-      if (index === j) return string[i];
-      return char;
-    })
-    .join('');
-};
-
 /**
  * @param {string} s
  * @param {string} goal
  * @return {boolean}
  */
 const buddyStrings = (s, goal) => {
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i + 1; j < s.length; j++) {
-      if (swap(s, i, j) === goal) return true;
-    }
+  if (s.length !== goal.length) return false;
+
+  const diff = getDiff(s, goal);
+
+  console.log(diff);
+
+  if (diff.length === 1 || diff.lenght > 2) return false;
+
+  if (diff.length === 2)
+    return s[diff[0]] === goal[diff[1]] && s[diff[1]] === goal[diff[0]];
+
+  return hasDuplicateChars(s);
+};
+
+const hasDuplicateChars = (s) => {
+  const chars = {};
+
+  for (const c of s) {
+    if (chars[c]) return true;
+    chars[c] = true;
   }
 
   return false;
 };
 
-console.log(buddyStrings('aa', 'aa'));
+const getDiff = (s, goal) => {
+  const diff = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== goal[i]) {
+      diff.push(i);
+    }
+  }
+
+  return diff;
+};
+
+console.log(buddyStrings('ab', 'ba'));
