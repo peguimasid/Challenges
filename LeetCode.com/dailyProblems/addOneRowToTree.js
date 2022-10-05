d9a8f829cd70ae3a60ad62cf5ba53542867f7494
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-05 11:26:01 -0300

    new challenge completed - add one row to tree - IMPROVING SOLUTION

--- LeetCode.com/dailyProblems/addOneRowToTree.js
+++ LeetCode.com/dailyProblems/addOneRowToTree.js
@@ -11,25 +11,16 @@ function TreeNode(val, left, right) {
  * @return {TreeNode}
  */
 const addOneRow = (root, val, depth) => {
-  if (depth === 1) {
-    const newRoot = new TreeNode(val);
-    newRoot.left = root;
-    return newRoot;
-  }
-  const traverse = (node, currentDepth = 1) => {
-    if (currentDepth > depth) return;
-    if (currentDepth === depth - 1) {
-      const newLeft = new TreeNode(val);
-      const newRight = new TreeNode(val);
-
-      newLeft.left = node.left;
-      newRight.right = node.right;
-      node.left = newLeft;
-      node.right = newRight;
+  if (depth === 1) return new TreeNode(val, root);
+  const traverse = (node, d = 1) => {
+    if (d === depth - 1) {
+      node.left = new TreeNode(val, node.left);
+      node.right = new TreeNode(val, null, node.right);
+      return;
     }
 
-    if (node?.left) traverse(node.left, currentDepth + 1);
-    if (node?.right) traverse(node.right, currentDepth + 1);
+    if (node?.left) traverse(node.left, d + 1);
+    if (node?.right) traverse(node.right, d + 1);
   };
 
   traverse(root);
@@ -43,4 +34,4 @@ bt.left.left = new TreeNode(3);
 bt.left.right = new TreeNode(1);
 bt.right.left = new TreeNode(5);
 
-console.log(addOneRow(bt, 1, 1));
+console.log(addOneRow(bt, 1, 2));
