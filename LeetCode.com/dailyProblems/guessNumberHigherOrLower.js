6d11bba629fbe1dae1da358f6eb7318a169b58ab
Guilhermo Masid <guilhermomasid@gmail.com>
2022-11-16 11:19:11 -0300

    new challenge completed - guess number - binary search divide and conquer solution

--- LeetCode.com/dailyProblems/guessNumberHigherOrLower.js
+++ LeetCode.com/dailyProblems/guessNumberHigherOrLower.js
@@ -7,16 +7,30 @@
  * var guess = function(num) {}
  */
 
+const guess = (num) => {
+  const pick = 6; // change this to the number you choose
+
+  if (num === pick) return 0;
+  if (num > pick) return -1;
+  if (num < pick) return 1;
+};
+
 /**
  * @param {number} n
  * @return {number}
  */
-var guessNumber = function (n) {
-  for (let i = 1; i <= n; i++) {
-    if (guess(i) === 0) return i;
-  }
+const guessNumber = (n) => {
+  if (guess(n) == 0) return n;
+  const binarySearch = (min, max) => {
+    let middle = Math.floor((min + max) / 2);
+    const guessResult = guess(middle);
+    if (guessResult === 0) return middle;
+    if (guessResult === 1) return binarySearch(middle, max);
+    if (guessResult === -1) return binarySearch(1, middle);
+  };
+  return binarySearch(1, n);
 };
 
 console.log(guessNumber(10));
-console.log(guessNumber(1));
-console.log(guessNumber(2));
+// console.log(guessNumber(1));
+// console.log(guessNumber(2));
