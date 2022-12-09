1d10204c6ed0401863b4bce0306510169596cce2
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-09 10:55:51 -0300

    new challenge completed - max ancestor diff

---
+++ LeetCode.com/dailyProblems/maximumDifferenceBetweenNodeAndAcestor.js
@@ -0,0 +1,37 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxAncestorDiff = (root) => {
+  if (!root) return 0;
+
+  let result = 0;
+
+  const traverse = (node, min, max) => {
+    if (!node) return (result = Math.max(result, max - min));
+
+    max = Math.max(max, node.val);
+    min = Math.min(min, node.val);
+
+    traverse(node.left, min, max);
+    traverse(node.right, min, max);
+  };
+
+  traverse(root, root.val, root.val);
+
+  return result;
+};
+
+// prettier-ignore
+const bt = new TreeNode(8, new TreeNode(3, new TreeNode(1), new TreeNode(6, new TreeNode(4), new TreeNode(7))), new TreeNode(10, null, new TreeNode(14, new TreeNode(13))));
+// prettier-ignore
+const bt2 = new TreeNode(1, null, new TreeNode(2, null, new TreeNode(0, new TreeNode(3))));
+
+console.log(maxAncestorDiff(bt));
+console.log(maxAncestorDiff(bt2));
