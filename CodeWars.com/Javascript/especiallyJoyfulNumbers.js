46034942cdc0ae9906fd479dc46a20e0bcfc957f
Guilhermo Masid <guilhermomasid@gmail.com>
2023-01-25 11:04:04 -0300

    new challenge completed - especially joyfull numbers

--- CodeWars.com/Javascript/especiallyJoyfulNumbers.js
+++ CodeWars.com/Javascript/especiallyJoyfulNumbers.js
@@ -6,6 +6,10 @@ const numberJoy = (n) => {
   const sumOfDigits = String(n)
     .split('')
     .reduce((acc, curr) => acc + Number(curr), 0);
+
+  return (
+    sumOfDigits * Number(String(sumOfDigits).split('').reverse().join('')) === n
+  );
 };
 
 console.log(numberJoy(1729));
