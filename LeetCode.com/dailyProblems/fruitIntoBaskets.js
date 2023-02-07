ad39ca10e8cb84ae7bcc6823082a9c9d607cbe87
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-07 11:41:22 -0300

    new challenge completed - fruits into basket - good on sol

--- LeetCode.com/dailyProblems/fruitIntoBaskets.js
+++ LeetCode.com/dailyProblems/fruitIntoBaskets.js
@@ -3,30 +3,23 @@
  * @return {number}
  */
 const totalFruit = (fruits) => {
+  if (new Set(fruits).size < 3) return fruits.length;
+
   let result = 0;
+  let before = 0;
 
   for (let i = 0; i < fruits.length; i++) {
-    let basketOne = null;
-    let basketTwo = null;
-    let currentCount = 0;
-
-    for (let j = i; j < fruits.length; j++) {
-      const currentFruit = fruits[j];
-      if (
-        basketOne !== null &&
-        basketTwo !== null &&
-        ![basketOne, basketTwo].includes(currentFruit)
-      ) {
-        break;
-      }
-      if (basketOne === null) {
-        basketOne = currentFruit;
-      } else if (basketTwo === null && currentFruit !== basketOne) {
-        basketTwo = currentFruit;
-      }
-      currentCount++;
+    if (fruits[i] === fruits[i + 1]) {
+      before++;
+      continue;
     }
-    if (currentCount > result) result = currentCount;
+    const set = new Set();
+    for (let j = i; j < fruits.length; j++) {
+      set.add(fruits[j]);
+      if (set.size === 2) result = Math.max(result, j - i + 1 + before);
+      if (set.size > 2) break;
+    }
+    before = 0;
   }
 
   return result;
