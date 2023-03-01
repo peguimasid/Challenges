55821dda796a2226f27ac14cd74c2237d55eb3ff
Guilhermo Masid <guilhermomasid@gmail.com>
2023-03-01 11:50:47 -0300

    new challenge completed - sort array - space and time good

--- LeetCode.com/dailyProblems/sortArray.js
+++ LeetCode.com/dailyProblems/sortArray.js
@@ -4,20 +4,22 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const quickSort = (nums) => {
-  if (nums.length <= 1) return nums;
-
-  const pivot = nums[0];
-
-  const left = [];
-  const right = [];
-
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] <= pivot) left.push(nums[i]);
-    if (nums[i] > pivot) right.push(nums[i]);
+const quickSort = (nums, start, end) => {
+  if (start >= end) return;
+  let left = start;
+  let right = end;
+  let pivot = nums[Math.floor((start + end) / 2)];
+  while (left <= right) {
+    while (left <= right && nums[left] < pivot) left++;
+    while (left <= right && nums[right] > pivot) right--;
+    if (left <= right) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left++;
+      right--;
+    }
   }
-
-  return [...quickSort(left), pivot, ...quickSort(right)];
+  quickSort(nums, start, right);
+  quickSort(nums, left, end);
 };
 
 /**
@@ -25,7 +27,9 @@ const quickSort = (nums) => {
  * @return {number[]}
  */
 const sortArray = (nums) => {
-  return quickSort(nums);
+  if (nums.length <= 1) return nums;
+  quickSort(nums, 0, nums.length - 1);
+  return nums;
 };
 
 console.log(sortArray([5, 3, 1, 2]));
