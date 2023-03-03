f21110b045d91b8b5ac23c7f0dae1b28af5e5546
Guilhermo Masid <guilhermomasid@gmail.com>
2023-03-03 13:39:31 -0300

    solution problem want me to do

--- LeetCode.com/dailyProblems/strstr.js
+++ LeetCode.com/dailyProblems/strstr.js
@@ -1,8 +1,28 @@
+// const strStr = (haystack, needle) => {
+//   return haystack.indexOf(needle);
+// };
+
 /**
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
 const strStr = (haystack, needle) => {
-  return haystack.indexOf(needle);
+  if (haystack === needle) return 0;
+  let i = 0;
+  while (i < haystack.length) {
+    let j = 0;
+    while (j < haystack.length) {
+      if (j === needle.length) return i;
+      if (haystack[i + j] !== needle[j]) break;
+      j++;
+    }
+    i++;
+  }
+
+  return -1;
 };
+
+console.log(strStr('sadbutsad', 'sad')); // 0
+console.log(strStr('idontknowwhattodo', 'know')); // 0
+console.log(strStr('leetcode', 'leeto')); // -1
