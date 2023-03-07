d34fc57d0c7ebd1dbb87812f8cd53deada060dca
Guilhermo Masid <guilhermomasid@gmail.com>
2023-03-07 12:05:21 -0300

    new challenge completed - minimum time

--- LeetCode.com/dailyProblems/minimumTime.js
+++ LeetCode.com/dailyProblems/minimumTime.js
@@ -4,15 +4,24 @@
  * @return {number}
  */
 const minimumTime = (time, totalTrips) => {
-  let t = 0;
+  let left = 0;
+  let right = Number.MAX_SAFE_INTEGER;
 
-  while (true) {
-    t++;
-    const currentTrips = time.map((time) => Math.floor(t / time));
-    const totalTime = currentTrips.reduce((acc, curr) => acc + curr, 0);
-    if (totalTime >= totalTrips) return t;
+  while (left < right) {
+    let mid = Math.floor((right + left) / 2);
+    let completedTrips = 0;
+    for (const t of time) {
+      completedTrips += Math.floor(mid / t);
+      if (completedTrips >= totalTrips) break;
+    }
+    if (completedTrips >= totalTrips) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
   }
+
+  return left;
 };
 
 console.log(minimumTime([1, 2, 3], 5));
-console.log(minimumTime([2], 1));
