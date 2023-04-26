912b1805c95220670099cdd45d01471f2bdd142a
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-26 11:01:12 -0300

    new challenge completed - add digits - ta de hack

--- LeetCode.com/dailyProblems/addDigits.js
+++ LeetCode.com/dailyProblems/addDigits.js
@@ -2,15 +2,7 @@
  * @param {number} num
  * @return {number}
  */
-const addDigits = (num) => {
-  while (num > 10) {
-    num = String(num)
-      .split('')
-      .reduce((acc, curr) => acc + Number(curr), 0);
-  }
-
-  return num;
-};
+const addDigits = (num) => ((num - 1) % 9) + 1;
 
 console.log(addDigits(38)); // 3 + 8 => 11 => 1 + 1 => 2
 console.log(addDigits(0)); // 0
