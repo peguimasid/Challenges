f3d69d66016f9de3fcc96f13a78e8cba23f644c0
Guilhermo Masid <guilhermomasid@gmail.com>
2023-05-20 14:53:38 -0300

    new challenge completed - moving zeros to the end - better solution

--- CodeWars.com/Javascript/movingZerosToTheEnd.js
+++ CodeWars.com/Javascript/movingZerosToTheEnd.js
@@ -3,8 +3,9 @@
  * @return {any[]}
  */
 function moveZeros(arr) {
-  const zeroCount = arr.filter((elem) => elem === 0).length;
-  return arr.filter((elem) => elem !== 0).concat(Array(zeroCount).fill(0));
+  return arr
+    .filter((elem) => elem !== 0)
+    .concat(arr.filter((elem) => elem === 0));
 }
 
 console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, 'a']));
