0e1daf8e24cf09d01526d0f707e6b5c01aea8915
Guilhermo Masid <guilhermomasid@gmail.com>
2023-05-24 11:34:31 -0300

    new challenge completed - alt digit sum - small but slow solution

--- LeetCode.com/problems/alternatingDigitSum.js
+++ LeetCode.com/problems/alternatingDigitSum.js
@@ -3,16 +3,10 @@
  * @return {number}
  */
 const alternateDigitSum = (n) => {
-  const str = String(n);
-
-  let result = 0;
-
-  for (let i = 0; i < str.length; i++) {
-    const num = Number(str[i]);
-    result += i % 2 === 0 ? num : -num;
-  }
-
-  return result;
+  return String(n)
+    .split('')
+    .map(Number)
+    .reduce((acc, curr, index) => (acc += index % 2 ? -curr : curr));
 };
 
 console.log(alternateDigitSum(521)); // 4
