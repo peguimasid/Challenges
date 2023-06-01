ab7285b90c5bebc43dc0b8c4b0bb103e17d2f485
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-01 11:31:18 -0300

    new challenge completed - shorted path in binary matrix - bfs

--- LeetCode.com/dailyProblems/shortestPathInBinaryMatrix.js
+++ LeetCode.com/dailyProblems/shortestPathInBinaryMatrix.js
@@ -5,35 +5,28 @@
 const shortestPathBinaryMatrix = (grid) => {
   const visitedNodes = new Set();
 
-  const dfs = (x, y) => {
-    if (x < 0 || x >= grid.length) return Infinity;
-    if (y < 0 || y >= grid[0].length) return Infinity;
+  const queue = [[0, 0, 1]];
+
+  while (queue.length > 0) {
+    const [x, y, distance] = queue.shift();
     const key = `${x}${y}`;
-    if (visitedNodes.has(key)) return Infinity;
-    if (grid[x][y] === 1) return Infinity;
-    if (x === grid.length - 1 && y === grid[0].length - 1) return 1;
-
+    if (visitedNodes.has(key)) continue;
     visitedNodes.add(key);
+    if (x < 0 || x >= grid.length) continue;
+    if (y < 0 || y >= grid[0].length) continue;
+    if (grid[x][y] === 1) continue;
+    if (x === grid.length - 1 && y === grid[0].length - 1) return distance;
+    queue.push([x - 1, y, distance + 1]);
+    queue.push([x + 1, y, distance + 1]);
+    queue.push([x, y - 1, distance + 1]);
+    queue.push([x, y + 1, distance + 1]);
+    queue.push([x - 1, y - 1, distance + 1]);
+    queue.push([x - 1, y + 1, distance + 1]);
+    queue.push([x + 1, y - 1, distance + 1]);
+    queue.push([x + 1, y + 1, distance + 1]);
+  }
 
-    const r1 = dfs(x - 1, y);
-    const r2 = dfs(x + 1, y);
-    const r3 = dfs(x, y - 1);
-    const r4 = dfs(x, y + 1);
-    const r5 = dfs(x - 1, y + 1);
-    const r6 = dfs(x + 1, y + 1);
-    const r7 = dfs(x + 1, y - 1);
-    const r8 = dfs(x - 1, y - 1);
-
-    visitedNodes.delete(key);
-
-    return Math.min(r1, r2, r3, r4, r5, r6, r7, r8) + 1;
-  };
-
-  const result = dfs(0, 0);
-
-  if (result === Infinity) return -1;
-
-  return result;
+  return -1;
 };
 
 // if start position is not "0" return -1;
