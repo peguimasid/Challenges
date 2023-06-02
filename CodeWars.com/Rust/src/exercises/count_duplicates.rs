b128d75782ddbed1062c7d623c816b3c71571e99
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-02 18:20:06 -0300

    new challenge completed - count duplicates rust - better solution

--- CodeWars.com/Rust/src/exercises/count_duplicates.rs
+++ CodeWars.com/Rust/src/exercises/count_duplicates.rs
@@ -4,14 +4,11 @@ pub fn count_duplicates(text: &str) -> u32 {
   text
     .to_lowercase()
     .chars()
-    .fold(HashMap::new(), |mut acc, curr| {
-      *acc.entry(curr.to_string()).or_insert(0) += 1;
+    .fold(HashMap::<char, usize>::new(), |mut acc, curr| {
+      *acc.entry(curr).or_insert(0) += 1;
       acc
     })
-    .into_iter()
-    .filter(|(_, value)| *value > 1)
-    .collect::<Vec<(String, u32)>>()
-    .len()
-    .try_into()
-    .unwrap()
+    .values()
+    .filter(|n| **n > 1)
+    .count() as u32
 }
