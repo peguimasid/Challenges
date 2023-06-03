be769d3ea8d75a6d6cdcaa38589780c14eb0f6fe
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-03 16:30:54 -0300

    finish day 1 part 2

--- AdventOfCode.com/aoc2022/src/day1/main.rs
+++ AdventOfCode.com/aoc2022/src/day1/main.rs
@@ -9,10 +9,14 @@ pub fn calorie_counting() -> u32 {
     })
     .collect();
 
-  let calories: Vec<u32> = elfs_calories
+  let mut calories: Vec<u32> = elfs_calories
     .into_iter()
     .map(|elf_calories| elf_calories.into_iter().sum())
     .collect();
 
-  *calories.iter().max().unwrap()
+  calories.sort_by(|a, b| b.cmp(a));
+
+  let test = calories.into_iter().take(3).collect::<Vec<u32>>();
+
+  test.into_iter().sum::<u32>()
 }
