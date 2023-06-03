50cb009d1c39ad050d405686e9fedf35db8ad71b
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-03 15:30:28 -0300

    new challenge completed - find outlier - rust

--- CodeWars.com/Rust/src/exercises/find_outlier.rs
+++ CodeWars.com/Rust/src/exercises/find_outlier.rs
@@ -1,4 +1,20 @@
 pub fn find_outlier(values: &[i32]) -> i32 {
-  println!("{:?}", values);
-  12
+  let vec = Vec::from(values);
+
+  let even: Vec<i32> = vec
+    .clone()
+    .into_iter()
+    .filter(|val| *val % 2 == 0)
+    .collect();
+  let odd: Vec<i32> = vec
+    .clone()
+    .into_iter()
+    .filter(|val| *val % 2 != 0)
+    .collect();
+
+  if odd.len() < even.len() {
+    odd[0]
+  } else {
+    even[0]
+  }
 }
--- CodeWars.com/Rust/src/main.rs
+++ CodeWars.com/Rust/src/main.rs
@@ -1,13 +1,13 @@
-#![allow(non_snake_case)]
-#[path = "exercises/count_duplicates.rs"]
+#[path = "exercises/find_outlier.rs"]
 mod my_mod;
 
 fn main() {
-  println!("{}", my_mod::count_duplicates("abcde")); // 0
-  println!("{}", my_mod::count_duplicates("aabbcde")); // 2
-  println!("{}", my_mod::count_duplicates("aabBcde")); // 2
-  println!("{}", my_mod::count_duplicates("indivisibility")); // 1
-  println!("{}", my_mod::count_duplicates("Indivisibilities")); // 2
-  println!("{}", my_mod::count_duplicates("aA11")); // 2
-  println!("{}", my_mod::count_duplicates("ABBA")); // 2
+  let t1 = [2, 6, 8, -10, 3];
+  let t2 = [
+    206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781,
+  ];
+  let t3 = [std::i32::MAX, 0, 1];
+  println!("{}", my_mod::find_outlier(&t1)); // 3
+  println!("{}", my_mod::find_outlier(&t2)); // 206847684
+  println!("{}", my_mod::find_outlier(&t3)); // 0
 }
