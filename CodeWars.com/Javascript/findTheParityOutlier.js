24460eaacd095296ee680e282ed665daf0547ec7
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-03 14:54:42 -0300

    new challenge completed - find outlier - best

--- CodeWars.com/Javascript/findTheParityOutlier.js
+++ CodeWars.com/Javascript/findTheParityOutlier.js
@@ -2,19 +2,10 @@
  * @param {number[]} integers
  */
 function findOutlier(integers) {
-  const frequency = integers.reduce(
-    (acc, curr) => {
-      if (Math.abs(curr) % 2 === 0) acc.even.push(curr);
-      if (Math.abs(curr) % 2 === 1) acc.odd.push(curr);
-      return acc;
-    },
-    { even: [], odd: [] }
-  );
+  const even = integers.filter((n) => n % 2 === 0);
+  const odd = integers.filter((n) => n % 2);
 
-  console.log(frequency);
-
-  const { even, odd } = frequency;
-  return even.length > odd.length ? odd[0] : even[0];
+  return even.length === 1 ? even[0] : odd[0];
 }
 
 console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
