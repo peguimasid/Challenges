ba1e02b6e3372adaffc78c3422af7c0304a5c6b6
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-05 10:34:03 -0300

    new challenge completed - ones and zeros - rust - better

--- CodeWars.com/Rust/src/main.rs
+++ CodeWars.com/Rust/src/main.rs
@@ -1,12 +1,9 @@
-#[path = "exercises/tribonacci_sequence.rs"]
+#[path = "exercises/ones_and_zeros.rs"]
 mod lib;
 
 fn main() {
-  println!("{:?}", lib::tribonacci(&[0., 1., 1.], 10)); //  [0., 1., 1., 2., 4., 7., 13., 24., 44., 81.]
-  println!("{:?}", lib::tribonacci(&[1., 0., 0.], 10)); //  [1., 0., 0., 1., 1., 2., 4., 7., 13., 24.]
-  println!("{:?}", lib::tribonacci(&[0., 0., 0.], 10)); //  [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]
-  println!("{:?}", lib::tribonacci(&[1., 2., 3.], 10)); //  [1., 2., 3., 6., 11., 20., 37., 68., 125., 230.]
-  println!("{:?}", lib::tribonacci(&[3., 2., 1.], 10)); //  [3., 2., 1., 6., 9., 16., 31., 56., 103., 190.]
-  println!("{:?}", lib::tribonacci(&[1., 1., 1.], 1)); //  [1.]
-  println!("{:?}", lib::tribonacci(&[300., 200., 100.], 0)); //  []
+  println!("{:?}", lib::binary_slice_to_number(&vec![0, 0, 0, 1])); //  1
+  println!("{:?}", lib::binary_slice_to_number(&vec![0, 0, 1, 0])); //  2
+  println!("{:?}", lib::binary_slice_to_number(&vec![1, 1, 1, 1])); //  15
+  println!("{:?}", lib::binary_slice_to_number(&vec![0, 1, 1, 0])); //  6
 }
