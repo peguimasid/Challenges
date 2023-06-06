6f934c68241cf3bd1bd3c74e24c54c0a8ca9f367
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-06 11:50:24 -0300

    new challenge completed - unique in order - rust - better

--- CodeWars.com/Rust/src/main.rs
+++ CodeWars.com/Rust/src/main.rs
@@ -1,9 +1,6 @@
-#[path = "exercises/ones_and_zeros.rs"]
+#[path = "exercises/unique_in_order.rs"]
 mod lib;
 
 fn main() {
-  println!("{:?}", lib::binary_slice_to_number(&vec![0, 0, 0, 1])); //  1
-  println!("{:?}", lib::binary_slice_to_number(&vec![0, 0, 1, 0])); //  2
-  println!("{:?}", lib::binary_slice_to_number(&vec![1, 1, 1, 1])); //  15
-  println!("{:?}", lib::binary_slice_to_number(&vec![0, 1, 1, 0])); //  6
+  println!("{:?}", lib::unique_in_order("AAAABBBCCDAABBB".chars())) // vec!['A','B','C','D','A','B']
 }
