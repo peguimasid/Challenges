b3c43d66ce597b28abd53ce34c5aee60ea63f174
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-07 14:38:32 -0300

    new challenge completed - can make arithmetic progression - better

--- LeetCode.com/dailyProblems/canMakeArithmeticProgressionFromSequence.js
+++ LeetCode.com/dailyProblems/canMakeArithmeticProgressionFromSequence.js
@@ -3,35 +3,15 @@
  * @return {boolean}
  */
 const canMakeArithmeticProgression = (arr) => {
-  const ascSorted = [...arr.sort((a, b) => a - b)];
-  const descSorted = [...arr.sort((a, b) => b - a)];
+  if (arr.length < 3) return true;
 
-  const diffAsc = ascSorted[0] - ascSorted[1];
-  const diffDesc = descSorted[0] - descSorted[1];
+  arr.sort((a, b) => a - b);
 
-  let result = true;
-
-  for (let i = 1; i < arr.length; i++) {
-    const current = ascSorted[i];
-    const previous = ascSorted[i - 1];
-
-    if (previous - current != diffAsc) {
-      result = false;
-      break;
-    }
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== arr[i - 1] - arr[i - 2]) return false;
   }
 
-  for (let i = 1; i < arr.length; i++) {
-    const current = descSorted[i];
-    const previous = descSorted[i - 1];
-
-    if (previous - current != diffDesc) {
-      result = false;
-      break;
-    }
-  }
-
-  return result;
+  return true;
 };
 
 console.log(canMakeArithmeticProgression([3, 5, 1])); // true
