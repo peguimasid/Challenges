a5e46d22aa5b131b4afb5c82a8addc760ca7f302
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-08 17:54:59 -0300

    new challenge completed - binet fibonacci

---
+++ Others/binetFibonacci.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const fib = (n) => {
+  return Math.floor(
+    (Math.pow((1 + Math.sqrt(5)) / 2, n) -
+      Math.pow((1 - Math.sqrt(5)) / 2, n)) /
+      Math.sqrt(5)
+  );
+};
+
+console.log(fib(2)); // 1
+console.log(fib(3)); // 2
+console.log(fib(4)); // 3
+console.log(fib(5)); // 5
+console.log(fib(6)); // 8
+console.log(fib(7)); // 13
