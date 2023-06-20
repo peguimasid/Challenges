dd4740dee4b09a5b873858f2c8e0ad842b1a0118
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-20 11:42:39 -0300

    new challenge completed - next bigger number - pass 141 of 142 testes

--- LeetCode.com/dailyProblems/nextBiggerNumberWithSameDigits.js
+++ LeetCode.com/dailyProblems/nextBiggerNumberWithSameDigits.js
@@ -1,46 +1,18 @@
 /**
- * @param {number} num
- * @return {object}
- */
-const getFrequency = (num) => {
-  const freq = {};
-  while (num > 0) {
-    freq[num % 10] = (freq[num % 10] || 0) + 1;
-    num = Math.floor(num / 10);
-  }
-  return freq;
-};
-
-/**
- * @param {object} original
- * @param {number} num
- * @return {boolean}
- */
-const hasSameFrequency = (original, num) => {
-  const freq = getFrequency(num);
-
-  for (const key in freq) {
-    if (original[key] != freq[key]) return false;
-  }
-
-  return true;
-};
-
-/**
  * @param {number} n
  * @return {number}
  */
-const nextBigger = (n) => {
-  const freq = getFrequency(n);
-
-  for (let i = n + 1; i < n * 10; i++) {
-    if (hasSameFrequency(freq, i)) {
-      return i;
-    }
+function nextBigger(n) {
+  const sortedDigits = (n) =>
+    String(n)
+      .split('')
+      .sort((a, b) => b - a);
+  const max = +sortedDigits(n).join('');
+  for (let i = n + 1; i <= max; i++) {
+    if (max === +sortedDigits(i).join('')) return i;
   }
-
   return -1;
-};
+}
 
 console.log(nextBigger(12), 21);
 console.log(nextBigger(513), 531);
