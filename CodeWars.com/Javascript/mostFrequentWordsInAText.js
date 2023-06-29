1e1ef37ea6d07c9b252db29eae04741c26cf9fbd
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-29 12:03:21 -0300

    remove comments

--- CodeWars.com/Javascript/mostFrequentWordsInAText.js
+++ CodeWars.com/Javascript/mostFrequentWordsInAText.js
@@ -28,18 +28,17 @@ console.log(
 
 console.log(topThreeWords("  //wont won't won't ")); // ["won't", 'wont']
 
-// console.log(topThreeWords('  , e   .. '), ['e']);
+console.log(topThreeWords('  , e   .. ')); //  ['e']
 
-// console.log(topThreeWords('  ...  '), []);
+console.log(topThreeWords('  ...  ')); // []
 
-// console.log(topThreeWords("  '  "), []);
+console.log(topThreeWords("  '  ")); // []
 
-// console.log(
-//   topThreeWords(`In a village of La Mancha, the name of which I have no desire to call to
-// mind, there lived not long since one of those gentlemen that keep a lance
-// in the lance-rack, an old buckler, a lean hack, and a greyhound for
-// coursing. An olla of rather more beef than mutton, a salad on most
-// nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
-// on Sundays, made away with three-quarters of his income.`),
-//   ['a', 'of', 'on']
-// );
+console.log(
+  topThreeWords(`In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income.`)
+); //['a', 'of', 'on']
