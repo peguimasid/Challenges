a15cc2539179aeb3666f4fa57a517f68ba4069e3
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-14 11:49:37 -0300

    new challenge completed - path finder - using bfs

--- CodeWars.com/Javascript/pathFinder.js
+++ CodeWars.com/Javascript/pathFinder.js
@@ -4,30 +4,39 @@
 function pathFinder(maze) {
   const mazeSplitted = maze.split('\n').map((row) => row.trim().split(''));
 
-  const visited = [];
+  const queue = [[0, 0]];
+  const visited = new Set(['0,0']);
 
-  const traverse = (x, y) => {
-    if (!mazeSplitted?.[x]?.[y] || mazeSplitted[x][y] === 'W') return false;
+  const directions = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
 
-    const key = `${x},${y}`;
+  while (queue.length > 0) {
+    const [x, y] = queue.shift();
 
-    if (visited.includes(key)) return false;
+    const n = mazeSplitted.length - 1;
+    const m = mazeSplitted[0].length - 1;
 
-    visited.push(key);
+    if (x === n && y === m) return true;
 
-    if (x === mazeSplitted.length - 1 && y === mazeSplitted[0].length - 1) {
-      return true;
+    for (const [dx, dy] of directions) {
+      const nx = x + dx;
+      const ny = y + dy;
+      const key = `${nx},${ny}`;
+
+      const exists = nx >= 0 && ny >= 0 && nx <= n && ny <= m;
+
+      if (exists && mazeSplitted[nx][ny] !== 'W' && !visited.has(key)) {
+        queue.push([nx, ny]);
+        visited.add(key);
+      }
     }
+  }
 
-    return (
-      traverse(x - 1, y) ||
-      traverse(x + 1, y) ||
-      traverse(x, y - 1) ||
-      traverse(x, y + 1)
-    );
-  };
-
-  return traverse(0, 0);
+  return false;
 }
 
 console.log(
