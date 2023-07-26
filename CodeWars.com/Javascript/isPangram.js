eb7c24d0621dea5199402dc6fe4074c442c4e3d7
Guilhermo Masid <guilhermomasid@gmail.com>
2023-07-26 11:08:01 -0300

    new challenge completed - is pangram - better solution

--- CodeWars.com/Javascript/isPangram.js
+++ CodeWars.com/Javascript/isPangram.js
@@ -3,18 +3,12 @@
  * @return {string}
  */
 function isPangram(string) {
-  return [
-    ...new Set(
-      string
-        .split('')
-        .map((char) => char.toLowerCase())
-        .filter((char) => 'abcdefghijklmnopqrstuvwxyz'.indexOf(char) > -1)
-        .sort()
-    ),
-  ]
-    .join('')
-    .startsWith('abcdefghijklmnopqrstuvwxyz');
+  string = string.toLowerCase();
+  return 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
+    .every((char) => string.includes(char));
 }
 
 console.log(isPangram('The quick brown fox jumps over the lazy dog.'));
 console.log(isPangram('This is not a pangram.'));
+console.log(isPangram('Cwm fjord bank glyphs vext quiz'));
