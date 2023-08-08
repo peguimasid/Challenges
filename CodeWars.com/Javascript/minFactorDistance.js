f765e894f2ea74902f9bba43e4606c200b7704a7
Guilhermo Masid <guilhermomasid@gmail.com>
2023-08-08 15:13:14 -0300

    new challenge completed - min factor distance

---
+++ CodeWars.com/Javascript/minFactorDistance.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function minDistance(n) {
+  const factors = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) factors.push(i);
+  }
+
+  let result = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 1; i < factors.length; i++) {
+    const prev = factors[i - 1];
+    const curr = factors[i];
+
+    if (curr - prev < result) {
+      result = curr - prev;
+    }
+  }
+
+  return result;
+}
+
+console.log(minDistance(13013));
