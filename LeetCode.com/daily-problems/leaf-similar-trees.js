5b150b37327a2812a5b4118ad4d6f3977848c629
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-09 12:43:27 -0300

    create file for leaf similar trees

---
+++ LeetCode.com/daily-problems/leaf-similar-trees.js
@@ -0,0 +1,12 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+function leafSimilar(root1, root2) {}
