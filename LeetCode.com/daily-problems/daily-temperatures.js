952939c86b9ba8752c3590765e6a54e45465eadc
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-31 13:50:29 -0300

    new challenge completed - daily temperatures - monotonic stack

--- LeetCode.com/daily-problems/daily-temperatures.js
+++ LeetCode.com/daily-problems/daily-temperatures.js
@@ -3,23 +3,17 @@
  * @return {number[]}
  */
 function dailyTemperatures(temperatures) {
-  const result = [];
+  const result = new Array(temperatures.length).fill(0);
+  const stack = [];
 
   for (let i = 0; i < temperatures.length; i++) {
-    const currentTemp = temperatures[i];
-    let push = false;
+    const temp = temperatures[i];
 
-    for (let j = i + 1; j < temperatures.length; j++) {
-      const nextTemp = temperatures[j];
-
-      if (nextTemp > currentTemp) {
-        result.push(j - i);
-        push = true;
-        break;
-      }
+    while (stack.length && temperatures[stack[stack.length - 1]] < temp) {
+      result[stack[stack.length - 1]] = i - stack.pop();
     }
 
-    if (!push) result.push(0);
+    stack.push(i);
   }
 
   return result;
@@ -27,6 +21,6 @@ function dailyTemperatures(temperatures) {
 
 console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); // [1,1,4,2,1,1,0,0]
 
-console.log(dailyTemperatures([30, 40, 50, 60])); // [1,1,1,0]
+// console.log(dailyTemperatures([30, 40, 50, 60])); // [1,1,1,0]
 
-console.log(dailyTemperatures([30, 60, 90])); // [1,1,0]
+// console.log(dailyTemperatures([30, 60, 90])); // [1,1,0]
