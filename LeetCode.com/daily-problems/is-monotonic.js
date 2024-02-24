1f6659cb8720415bb6c4e63f2942192030770bca
Guilhermo Masid <guilhermomasid@gmail.com>
2024-02-24 16:05:46 -0300

    improve memory usage

--- LeetCode.com/daily-problems/is-monotonic.js
+++ LeetCode.com/daily-problems/is-monotonic.js
@@ -7,10 +7,8 @@ function isMonotonic(nums) {
   let isDecreasing = true;
 
   for (let i = 1; i < nums.length; i++) {
-    const prev = nums[i - 1];
-    const curr = nums[i];
-    if (prev < curr) isDecreasing = false;
-    if (prev > curr) isIncreasing = false;
+    if (nums[i - 1] < nums[i]) isDecreasing = false;
+    if (nums[i - 1] > nums[i]) isIncreasing = false;
   }
 
   return isIncreasing || isDecreasing;
