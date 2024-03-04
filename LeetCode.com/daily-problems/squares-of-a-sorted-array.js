58cc8b65270a0fc84488ad3c704634fecdf9dbc4
Guilhermo Masid <guilhermomasid@gmail.com>
2024-03-04 17:31:50 -0300

    using o(n) solution

--- LeetCode.com/daily-problems/squares-of-a-sorted-array.js
+++ LeetCode.com/daily-problems/squares-of-a-sorted-array.js
@@ -3,9 +3,23 @@
  * @return {number[]}
  */
 function sortedSquares(nums) {
-  return nums
-    .toSorted((a, b) => Math.abs(a) - Math.abs(b))
-    .map((num) => num ** 2);
+  const N = nums.length;
+  const result = new Array(N).fill(0);
+
+  let start = 0;
+  let end = N - 1;
+
+  for (let i = N - 1; i >= 0; i--) {
+    if (Math.abs(nums[start]) >= Math.abs(nums[end])) {
+      result[i] = nums[start] * nums[start];
+      start++;
+    } else {
+      result[i] = nums[end] * nums[end];
+      end--;
+    }
+  }
+
+  return result;
 }
 
 console.log(sortedSquares([-4, -1, 0, 3, 10])); // [0,1,9,16,100]
