2656b2c7cd3889d663492a65f5a6260d06efafa0
Guilhermo Masid <guilhermomasid@gmail.com>
2024-03-10 19:25:20 -0300

    small solution

--- LeetCode.com/daily-problems/intersection-of-two-arrays.js
+++ LeetCode.com/daily-problems/intersection-of-two-arrays.js
@@ -4,17 +4,9 @@
  * @return {number}
  */
 function intersection(nums1, nums2) {
-  const result = new Set();
-
-  const s2 = new Set(nums2);
-
-  for (const num of nums1) {
-    if (s2.has(num) && !result.has(num)) {
-      result.add(num);
-    }
-  }
-
-  return [...result];
+  const set1 = new Set(nums1);
+  const set2 = new Set(nums2);
+  return [...new Set([...set1].filter((num) => set2.has(num)))];
 }
 
 console.log(intersection([1, 2, 2, 1], [2, 2])); // [2]
