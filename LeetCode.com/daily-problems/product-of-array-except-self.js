e1225bd6afa2fd74f22d13f7fa446d99a41bc3c7
Guilhermo Masid <guilhermomasid@gmail.com>
2024-03-15 12:53:45 -0300

    new challenge completed - product of array except self - prefix sum solution

--- LeetCode.com/daily-problems/product-of-array-except-self.js
+++ LeetCode.com/daily-problems/product-of-array-except-self.js
@@ -3,18 +3,23 @@
  * @return {number[]}
  */
 function productExceptSelf(nums) {
+  const n = nums.length;
+
+  const prefix = new Array(n).fill(1);
+  const suffix = new Array(n).fill(1);
+
+  for (let i = 1; i < n; i++) {
+    prefix[i] = prefix[i - 1] * nums[i - 1];
+  }
+
+  for (let i = n - 2; i >= 0; i--) {
+    suffix[i] = suffix[i + 1] * nums[i + 1];
+  }
+
   const result = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    let product = 1;
-
-    for (let j = 0; j < i; j++) {
-      product *= nums[j];
-    }
-    for (let j = i + 1; j < nums.length; j++) {
-      product *= nums[j];
-    }
-    result.push(product === 0 ? 0 : product);
+  for (let i = 0; i < n; i++) {
+    result.push(prefix[i] * suffix[i]);
   }
 
   return result;
