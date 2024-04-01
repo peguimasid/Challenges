41fea755c030bce8bf92d98331f3868edc3dd873
Guilhermo Masid <guilhermomasid@gmail.com>
2024-03-31 21:53:48 -0300

    new challenge completed - length of last word - faster

--- LeetCode.com/daily-problems/length-of-last-word.js
+++ LeetCode.com/daily-problems/length-of-last-word.js
@@ -3,7 +3,17 @@
  * @return {number}
  */
 function lengthOfLastWord(s) {
-  return s.trim().split(' ').pop().length;
+  let result = 0;
+
+  let i = s.length - 1;
+
+  while (s[i] && s[i] === ' ') i--;
+  while (s[i] && s[i] !== ' ') {
+    i--;
+    result++;
+  }
+
+  return result;
 }
 
 console.log(lengthOfLastWord('Hello World')); //  5
