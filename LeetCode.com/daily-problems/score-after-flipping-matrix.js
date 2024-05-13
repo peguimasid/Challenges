7aec7fc818fea62adabba960c4cf50b7bd279276
Guilhermo Masid <guilhermomasid@gmail.com>
2024-05-13 12:03:39 -0300

    hacky solution

--- LeetCode.com/daily-problems/score-after-flipping-matrix.js
+++ LeetCode.com/daily-problems/score-after-flipping-matrix.js
@@ -1,61 +1,22 @@
 /**
  * @param {number[][]} grid
- * @param {number} index
- * @return {number[]}
- */
-function getCol(grid, index) {
-  const result = [];
-
-  for (const row of grid) {
-    result.push(row[index]);
-  }
-
-  return result;
-}
-
-/**
- * @param {number[]} column
- * @return {object}
- */
-function countFrequency(column) {
-  const result = { zeroes: 0, ones: 0 };
-
-  for (const value of column) {
-    if (value === 0) result.zeroes++;
-    if (value === 1) result.ones++;
-  }
-
-  return result;
-}
-
-/**
- * @param {number[][]} grid
  * @return {number}
  */
 function matrixScore(grid) {
-  // for each row, if starts with zero, flip
-  for (let i = 0; i < grid.length; i++) {
-    const row = grid[i];
-    if (row[0] === 0) {
-      grid[i] = row.map((i) => (i === 0 ? 1 : 0));
+  const n = grid.length;
+  const m = grid[0].length;
+
+  let result = 2 ** (m - 1) * n;
+
+  for (let c = 1; c < m; c++) {
+    let curr = 0;
+    for (let r = 0; r < n; r++) {
+      curr += grid[r][0] === grid[r][c] ? 1 : 0;
     }
+    result += Math.max(curr, n - curr) * 2 ** (m - 1 - c);
   }
 
-  // for each column, if has mores zeroes than ones, flip
-  for (let i = 0; i < grid[0].length; i++) {
-    const column = getCol(grid, i);
-    const { ones, zeroes } = countFrequency(column);
-    if (zeroes > ones) {
-      for (const row of grid) {
-        row[i] = row[i] === 0 ? 1 : 0;
-      }
-    }
-  }
-
-  return grid.reduce((acc, row) => {
-    acc += parseInt(row.join(''), 2);
-    return acc;
-  }, 0);
+  return result;
 }
 
 const grid1 = [
