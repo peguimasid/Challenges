926a66151f6dc9cd327db1c15dc1fb637d7cd865
Guilhermo Masid <guilhermomasid@gmail.com>
2024-05-24 04:38:07 -0300

    new challenge completed - disvowel

---
+++ CodeWars.com/Javascript/disvowel-trolls.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+function disemvowel(str) {
+  return str.replaceAll(/[aeiouAEIOU]/g, '');
+}
+
+console.log(disemvowel('This website is for losers LOL!'));
+// "Ths wbst s fr lsrs LL!"
+
+console.log(
+  disemvowel("No offense but,\nYour writing is among the worst I've ever read")
+);
+// "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
+
+console.log(disemvowel('What are you, a communist?'));
+// "Wht r y,  cmmnst?"
