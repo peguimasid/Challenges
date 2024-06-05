747575a0726c168b020d05ef7dd27d2dc3b5394c
Guilhermo Masid <guilhermomasid@gmail.com>
2024-06-05 11:35:07 -0300

    o(n x m) solution

--- LeetCode.com/daily-problems/find-common-characters.js
+++ LeetCode.com/daily-problems/find-common-characters.js
@@ -1,43 +1,27 @@
 /**
  * @param {string[]} words
- * @return {object[]}
- */
-function getWordsFrequencies(words) {
-  const result = [];
-
-  for (const word of words) {
-    const frequency = {};
-
-    for (const char of word) {
-      frequency[char] = (frequency[char] || 0) + 1;
-    }
-
-    result.push(frequency);
-  }
-
-  return result;
-}
-
-/**
- * @param {string[]} words
  * @return {string[]}
  */
 function commonChars(words) {
-  const frequencies = getWordsFrequencies(words);
+  const minimumFrequency = new Array(26).fill(Infinity);
 
-  const chars = Object.keys(frequencies[0]);
+  for (const word of words) {
+    const frequency = new Array(26).fill(0);
 
-  let result = [];
-
-  for (const char of chars) {
-    let amountToPush = Number.MAX_SAFE_INTEGER;
-
-    for (const frequency of frequencies) {
-      amountToPush = Math.min(amountToPush, frequency[char] || 0);
+    for (const char of word) {
+      frequency[char.charCodeAt() - 97]++;
     }
 
-    for (let i = 0; i < amountToPush; i++) {
-      result.push(char);
+    for (let i = 0; i < 26; i++) {
+      minimumFrequency[i] = Math.min(minimumFrequency[i], frequency[i]);
+    }
+  }
+
+  const result = [];
+
+  for (let i = 0; i < 26; i++) {
+    for (let j = 0; j < minimumFrequency[i]; j++) {
+      result.push(String.fromCharCode(i + 97));
     }
   }
 
