4f8d599cede7d656365164a8c428e429a7921fa8
Guilhermo Masid <guilhermomasid@gmail.com>
2024-08-12 11:51:08 -0300

    kth largest - tle 9/10

---
+++ CodeWars.com/Javascript/kth-largest.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+class KthLargest {
+  constructor(k, nums) {
+    this.nums = nums;
+    this.k = k;
+  }
+  /**
+   * @param {number} val
+   * @return {number}
+   */
+  add(val) {
+    this.nums = [...this.nums, val].sort((a, b) => b - a);
+    return this.nums[this.k - 1];
+  }
+}
+
+const obj = new KthLargest(3, [4, 5, 8, 2]);
+
+console.log(obj.add(3)); // 4
+console.log(obj.add(5)); // 5
+console.log(obj.add(10)); // 5
+console.log(obj.add(9)); // 8
+console.log(obj.add(4)); // 8
