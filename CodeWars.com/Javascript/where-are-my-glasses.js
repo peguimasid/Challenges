f5e468cdd937f35982e30299eb71e9723a2627b8
Guilhermo Masid <guilhermomasid@gmail.com>
2024-08-29 11:14:43 -0300

    new challenge completed - where are my glasses

---
+++ CodeWars.com/Javascript/where-are-my-glasses.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} arr
+ * @return {number}
+ */
+function findGlasses(arr) {
+  return arr.findIndex((item) => /O-+O/.test(item));
+}
+
+console.log(findGlasses(["phone", "O-O", "coins", "keys"])); // 1
+console.log(findGlasses(["OO", "wallet", "O##O", "O----O"])); // 3
+console.log(findGlasses(["O_O", "O-O", "OwO"])); // 1
+console.log(findGlasses(["O--#--O", "dustO---Odust", "more dust"])); // 1
+// prettier-ignore
+console.log(findGlasses(['floor', 'the floor again', 'pockets', 'bed', 'cabinet', 'the top of my head O-O'])) // 5
+console.log(findGlasses(["OOOO----~OOO", "-------", "OOOOOOO", "OO-OO-OO-O"])); // 3
