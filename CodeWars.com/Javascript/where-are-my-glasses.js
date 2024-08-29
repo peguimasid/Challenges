fbe01cab6189d1867015737d7855a8857c7d522c
Guilhermo Masid <guilhermomasid@gmail.com>
2024-08-29 11:15:43 -0300

    remove prettier ignore

--- CodeWars.com/Javascript/where-are-my-glasses.js
+++ CodeWars.com/Javascript/where-are-my-glasses.js
@@ -10,6 +10,14 @@ console.log(findGlasses(["phone", "O-O", "coins", "keys"])); // 1
 console.log(findGlasses(["OO", "wallet", "O##O", "O----O"])); // 3
 console.log(findGlasses(["O_O", "O-O", "OwO"])); // 1
 console.log(findGlasses(["O--#--O", "dustO---Odust", "more dust"])); // 1
-// prettier-ignore
-console.log(findGlasses(['floor', 'the floor again', 'pockets', 'bed', 'cabinet', 'the top of my head O-O'])) // 5
+console.log(
+  findGlasses([
+    "floor",
+    "the floor again",
+    "pockets",
+    "bed",
+    "cabinet",
+    "the top of my head O-O",
+  ])
+); // 5
 console.log(findGlasses(["OOOO----~OOO", "-------", "OOOOOOO", "OO-OO-OO-O"])); // 3
